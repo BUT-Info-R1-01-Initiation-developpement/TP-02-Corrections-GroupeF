b1b1e4de7090d04b86b576b0187a999d6012a702
Franck Silvestre <franck.silvestre@gmail.com>
2025-09-15 16:51:44 +0200

    fix #1 Programme Bye commenté.

--- src/main/kotlin/tp02/activite02/01_Bye.kt
+++ src/main/kotlin/tp02/activite02/01_Bye.kt
@@ -3,15 +3,17 @@ package tp02.activite02     // indique qu'on se trouve dans un "espace de nom"
 
 fun main() {
     println("Saisis un texte et appuie sur entrée...")
-    // ...
+    // Récupération du texte saisi par l'utilisateur
     var saisieUtilisateur = readln()
     println("Tu as saisi : $saisieUtilisateur")
-    // ...
+    // Tant que l'utilisateur ne saisie pas "bye"
+    // on affiche ce qu'a saisi l'utilisateur.
+    // S'il a saisi "bye", on affiche "bye" et on sort de boucle
     while (saisieUtilisateur != "bye") {
         println("Saisis un texte et appuie sur entrée...")
         saisieUtilisateur = readln()
         println("Tu as saisi : $saisieUtilisateur")
     }
-    // ...
+    // On affiche au revoir
     println("Au revoir")
 }
