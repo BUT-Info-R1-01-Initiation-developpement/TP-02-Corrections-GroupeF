c52fe5a7fad8594f556148befe62ce5318e17525
Franck Silvestre <franck.silvestre@gmail.com>
2025-09-16 08:43:43 +0200

    fix #4 Programme Compte les mots commenté.

--- src/main/kotlin/tp02/activite02/04_CompteLesMots.kt
+++ src/main/kotlin/tp02/activite02/04_CompteLesMots.kt
@@ -3,13 +3,13 @@ package tp02.activite02
 fun main() {
     println("Saisie le mot dont tu veux compter les occurrences :")
     val motCompté = readln()
-    var nombreOccurrences = 0 // .............. nombreOccurences à 0
-    var motSaisi: String  // ............ la variable motSaisi
+    var nombreOccurrences = 0 // Déclare et initialise nombreOccurences à 0
+    var motSaisi: String  // Déclare la variable motSaisi
     do {
         println("Saisie un mot :")
         motSaisi = readln()
         if (motSaisi == motCompté) {
-            nombreOccurrences++  // ............. le nombre occurrences.
+            nombreOccurrences++  // Incrémente le nombre occurrences.
         }
     } while(motSaisi != "bye")
     println("Le mot \"$motCompté\" a été saisi $nombreOccurrences fois")
@@ -17,9 +17,9 @@ fun main() {
 
 // Dans ce contexte, nombreOccurrences++ peut s'écrire aussi
 // de la manière suivante :
-// nombreOccurences = ................ + .....
+// nombreOccurences = nombreOccurences + 1
 // ou encore
-// nombreOccurences ... 1
+// nombreOccurences += 1
 
 // Pourquoi est-on obligé de déclarer le type de la variable motSaisi à sa déclaration ?
-// ...
+// Car on assigne aucune valeur (aucun objet) à la variable motSaisi au moment de la déclaration.
