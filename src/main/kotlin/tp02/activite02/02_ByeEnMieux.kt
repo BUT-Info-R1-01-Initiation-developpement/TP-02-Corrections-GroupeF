34043513a4d591661975f2b39c30caf036a16581
Franck Silvestre <franck.silvestre@gmail.com>
2025-09-16 08:10:57 +0200

    fix #2 Programme Bye en mieux commenté.

--- src/main/kotlin/tp02/activite02/02_ByeEnMieux.kt
+++ src/main/kotlin/tp02/activite02/02_ByeEnMieux.kt
@@ -3,7 +3,10 @@ package tp02.activite02
 fun main() {
     // Pourquoi ce code peut-il être considéré comme "meilleur"
     // que le code du fichier `01_Bye.kt` ?
-    // ...
+    // Ce code permet d'éviter la duplication du code permettant la première saisie
+    // de l'utilisateur.
+    // Le do-while en exécutant au moins une fois le contenu du bloc de code la boucles
+    // est particulièrement adapté à cette situation.
     var saisieUtilisateur: String
     do {
         println("Saisis un texte et appuie sur entrée...")
