2d2ace1d8b090a2a47fba12ef8bc913d3c7f6719
Franck Silvestre <franck.silvestre@gmail.com>
2025-09-16 08:24:34 +0200

    fix #3 Programme Somme entiers commenté.

--- src/main/kotlin/tp02/activite02/03_SommeEntiers.kt
+++ src/main/kotlin/tp02/activite02/03_SommeEntiers.kt
@@ -3,18 +3,18 @@ package tp02.activite02
 fun main() {
     println("Saisie un nombre entier ou bye pour arrêter le programme : ")
     var valeurSaisie = readln()
-    var somme = 0 // .......... la variable somme à 0
+    var somme = 0 // déclare et intialise la variable somme à 0
     while(valeurSaisie != "bye") {
         // L'instruction somme += valeurSaisie.toInt() est équivalente à celle-ci :
-        // somme = ........ + ..........
+        // somme = somme + valeurSaisie.toInt()
         // Cette instruction fait plusieurs choses :
-        // 1. l'objet référencé par la variable valeurSaisie est ..............
+        // 1. l'objet référencé par la variable valeurSaisie est converti
         // en un objet de type Int ;
         // 2. L'objet de type Int obtenu est ajouté à l'objet de type Int référencé
-        // par la variable ............ ;
-        // 3. Le résultat est assigné la variable ...................
+        // par la variable somme ;
+        // 3. Le résultat est assigné la variable somme.
         somme += valeurSaisie.toInt()
-        // Affiche ....
+        // Affiche la somme des nombres saisis
         println("Somme des nombres saisis : $somme")
 
         // Saisie d'une nouvelle entrée par l'utilisateur
