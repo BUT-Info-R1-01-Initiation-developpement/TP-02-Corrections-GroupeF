9740a5619f37513a345eadc4da048ceab3e6bd06
Franck Silvestre <franck.silvestre@gmail.com>
2025-09-22 16:26:18 +0200

    fix #7 Programme TrouveMax : deuxième fix

--- src/main/kotlin/tp02/activite03/02_TrouveMax.kt
+++ src/main/kotlin/tp02/activite03/02_TrouveMax.kt
@@ -3,29 +3,30 @@ package tp02.activite03
 // Cette fonction doit demander à l'utilisateur de saisir des nombres
 // jusqu'à ce qu'il saisisse 0, puis afficher le plus grand nombre saisi
 fun trouveMax() {
-    /*
-    var nombreMax = 0
-    var nombreSaisi: Int
-    
+    println("Entrez un nombre (0 pour terminer):")
+    var nombreSaisi = readln().toInt()
+    var nombreMax = nombreSaisi
     while (nombreSaisi != 0) {
         println("Entrez un nombre (0 pour terminer):")
         nombreSaisi = readln().toInt()
-        
-        if (nombreSaisi > nombreMax) {
+        if (nombreSaisi != 0 && nombreSaisi > nombreMax) {
             nombreMax = nombreSaisi
         }
     }
-    
-    println("Le plus grand nombre saisi (hors 0) est: $nombreMax")
-     */
+    if (nombreMax == 0) {
+        println("Pas de nombre saisi différent de 0")
+    } else {
+        println("Le plus grand nombre saisi (hors 0) est: $nombreMax")
+    }
 }
+
 
 fun main() {
     trouveMax()
 }
 
 // Identifiez l'erreur de compilation et expliquez-la en français.
-// ...
+// Variable utilisé&e alors qu'elle n'était pas assignée.
 
 // Que se passe-t-il si l'utilisateur entre uniquement des nombres négatifs puis 0 ?
 // ....
